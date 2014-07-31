2f42d50b2b1270380af4366ec86f43abdfe47804
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-07-31 15:33:36 +0200

    prepared for share

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name 'application_buildpack'
-license 'MIT'
-description 'deploys and configures apps using heroku buildpacks'
 version '0.0.1'
+license 'MIT'
+
+maintainer '256dpi'
+
+description 'deploys and configures apps using heroku buildpacks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 depends 'monit'
 depends 'application', '~> 4.0'
